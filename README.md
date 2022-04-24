be0dc88025a412de62afd04960952d0d4d5e3af7
suyash-more <suyashmore789@gmail.com>
2022-04-24 18:45:06 +0530

    this is the initila push to the repo

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Online-Judge
-This is a Online judge for evaluating the code submitted by the user. 
+![image](https://user-images.githubusercontent.com/28949397/117579075-fdc2e980-b10e-11eb-9019-bc27c29b6511.png)
---
+++ backend/index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb://localhost/compilerdb",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    err && console.error(err);
+    console.log("Successfully connected to MongoDB: compilerdb");
+  }
+);
+
+const { generateFile } = require("./generateFile");
+
+const { addJobToQueue } = require("./jobQueue");
+const Job = require("./models/Job");
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/run", async (req, res) => {
+  const { language = "cpp", code } = req.body;
+
+  console.log(language, "Length:", code.length);
+
+  if (code === undefined) {
+    return res.status(400).json({ success: false, error: "Empty code body!" });
+  }
+  // need to generate a c++ file with content from the request
+  const filepath = await generateFile(language, code);
+  // write into DB
+  const job = await new Job({ language, filepath }).save();
+  const jobId = job["_id"];
+  addJobToQueue(jobId);
+  res.status(201).json({ jobId });
+});
+
+app.get("/status", async (req, res) => {
+  const jobId = req.query.id;
+
+  if (jobId === undefined) {
+    return res
+      .status(400)
+      .json({ success: false, error: "missing id query param" });
+  }
+
+  const job = await Job.findById(jobId);
+
+  if (job === undefined) {
+    return res.status(400).json({ success: false, error: "couldn't find job" });
+  }
+
+  return res.status(200).json({ success: true, job });
+});
+
+app.listen(5000, () => {
+  console.log(`Listening on port 5000!`);
+});
